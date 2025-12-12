4e692eaf876a34e7e76a0e1ee1f199b20f3f3d09
Tomasz Slomian <tomasz.slomian@gmail.com>
2025-12-12 14:16:26 +0100

    Fix: Use docs folder for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
       base: '/Data_Maturity/',
+      build: {
+        outDir: 'docs',
+      },
       server: {
         port: 3000,
         host: '0.0.0.0',
